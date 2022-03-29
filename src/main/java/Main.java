9d63aa021a7f6611027ebe711b550a71a6e75983
Sivolapyy <mzhitnov@yandex.ru>
2022-03-29 08:37:03 +0500

    try\catch added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,32 +6,38 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(10000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
-                .build();
+                .build()) {
 
-        HttpGet request = new HttpGet(
-                "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+            HttpGet request = new HttpGet(
+                    "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
 
-        CloseableHttpResponse response = httpClient.execute(request);
+            CloseableHttpResponse response = httpClient.execute(request);
 
-        ObjectMapper mapper = new ObjectMapper();
-        List<CatsFacts> catsFacts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
-        });
+            ObjectMapper mapper = new ObjectMapper();
+            List<CatsFacts> catsFacts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+            });
 
-        catsFacts.stream().filter(fact -> fact.getUpvotes() != null && fact.getUpvotes() > 0)
-                .forEach(fact -> System.out.println(fact.getText() +
-                        " (голосов - " + fact.getUpvotes() + ")"));
+            catsFacts.stream().filter(fact -> fact.getUpvotes() != null && fact.getUpvotes() > 0)
+                    .forEach(fact -> System.out.println(fact.getText() +
+                            " (голосов - " + fact.getUpvotes() + ")"));
+
+        } catch (IOException exc) {
+            System.out.println("Ошибка - " + exc.getMessage());
+        }
+
     }
 
 }
